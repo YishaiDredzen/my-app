bbd065c2ecb9fda3f8b76569cd05ed187c234773
Avi Soclof <soclofa@gmail.com>
2021-12-01 08:58:38 +0200

    search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/button'
+import SearchSection from './components/searchSection'
 
-function App() {
+const App = () => {
 
 const results = [
   { text: "wars", filename: "war.mp3"},
@@ -10,14 +11,36 @@ const results = [
   { text: "states", filename: "state.mp3"},
 ]
 
-let videoSource = "https://www.youtube.com/embed/mFQYSxWn7hU";
+let videoId = "mFQYSxWn7hU";
+
+
+const ApiUrl = "https://www.google.com/search?q="
+
+const search = (searchString) => {
+  fetch(ApiUrl+searchString)
+    .then(response => {
+        // handle the response
+    })
+    .catch(error => {
+        // handle the error
+    });
+}
+
+// const searchText = ''
+const [searchText, setSearchText] = useState('')
+
+const inputSearchChars = (char) => {
+// const [searchText, setSearchText] = useState('')
+  setSearchText(searchText+char)
+  // searchText+=char
+}
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <input type="text" className="search-box"></input>
-          <Button text="Search" />
+          <input type="text" className="search-box" value={searchText} onKeyDown={event => inputSearchChars(event.key)} />
+          <button onClick={() => search(searchText)}>Search</button>
           <div>
             <ul>
               {results.map(result => <li><a href="">{result.text}</a></li>)}
@@ -26,7 +49,7 @@ let videoSource = "https://www.youtube.com/embed/mFQYSxWn7hU";
         </div>
       </header>
       <main>
-      <iframe width="560" height="315" src={videoSource} title="Toldos Yisrael" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+videoId} title="Toldos Yisrael" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <ul>
           <li>list item...</li>
         </ul>
---
+++ src/components/searchSection.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const SearchSection = ({ text }) => {
+  return <button>{text}</button>;
+};
+
+export default SearchSection;
