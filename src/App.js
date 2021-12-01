47a55ab0d962811e0da011ed49b6ca0d014e43f8
Yishai Dredzen <yishaid@live.com>
2021-12-01 02:00:20 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/button'
 
 function App() {
 
@@ -9,34 +10,27 @@ const results = [
   { text: "state", filename: "state.mp3"},
 ]
 
+let videoSource = "https://www.youtube.com/embed/mFQYSxWn7hU";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          <input type="text"></input>
-      
-          <button>
-            Enter
-          </button>
+        <div>
+          <input type="text" className="search-box"></input>
+          <Button text="Search" />
           <div>
             <ul>
               {results.map(result => <li><a href="">{result.text}</a></li>)}
             </ul>
           </div>
-        </p>
+        </div>
       </header>
+      <main>
+      <iframe width="560" height="315" src={videoSource} title="Toldos Yisrael" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <ul>
+          <li>list item...</li>
+        </ul>
+      </main>
     </div>
   );
 }
